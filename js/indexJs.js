61abefe6d99cb9d513fa10f9daccf5180e0537e4
[edvaldinhs] <[edvaldohenriqueceu@gmail.com]>
2024-04-02 08:19:13 -0300

    First Commit

---
+++ js/indexJs.js
@@ -0,0 +1,184 @@
+document.addEventListener('DOMContentLoaded', function () {
+    let header = document.querySelector('.header-container');
+    header.classList.add("show-header");
+
+    const textElement = document.querySelector('.sec-text');
+    const words = ['MindBlind!', 'Mind Blind!', 'Blind Mind!', 'MB!'];
+
+    let wordIndex = 0;
+    let isReversed = false;
+    let charIndex = 0;
+
+    function updateText() {
+        const currentWord = words[wordIndex];
+        textElement.textContent = currentWord.substring(0, charIndex);
+
+        if (!isReversed) {
+            charIndex++;
+
+            if (charIndex > currentWord.length) {
+                isReversed = true;
+                setTimeout(updateText, 1000);
+            } else {
+                setTimeout(updateText, 100);
+            }
+        } else {
+            charIndex--;
+
+            if (charIndex === 0) {
+                isReversed = false;
+                wordIndex = (wordIndex + 1) % words.length;
+                setTimeout(updateText, 1000);
+            } else {
+                setTimeout(updateText, 100);
+            }
+        }
+    }
+
+    let headerWrapper = document.querySelector('.header-wrapper');
+    let sticky = header.offsetTop;
+    let scrollThreshold = 500;
+
+    window.addEventListener('scroll', () => {
+        if (window.pageYOffset > scrollThreshold) {
+            header.classList.add('sticky');
+            headerWrapper.style.height = '80px';
+        } else {
+            header.classList.remove('sticky');
+            headerWrapper.style.height = '0';
+        }
+    });
+
+    updateText();
+
+    const list = document.querySelector('.horizontal-carousel ul');
+    const items = document.querySelectorAll('.horizontal-carousel li');
+    let isDragging = false;
+    let startPosition = 0;
+    let startTranslate = 0;
+    let currentTranslate = 0;
+    let clickStartPosition = 0;
+
+    function handleItemClick(selectedItem) {
+        items.forEach(item => {
+            item.classList.remove('selected');
+            item.style.filter = 'grayscale(100%)';
+        });
+
+        selectedItem.parentElement.classList.add('selected');
+        selectedItem.parentElement.style.filter = 'none';
+
+        const selectedIndex = Array.from(items).indexOf(selectedItem.parentElement);
+
+        const translateValue = -selectedIndex * selectedItem.parentElement.offsetWidth + (list.offsetWidth / 2 - selectedItem.parentElement.offsetWidth / 2);
+
+        list.style.transform = `translateX(${translateValue}px)`;
+    }
+
+    handleItemClick(items[0].querySelector('.item'));
+
+    list.addEventListener('mousedown', (e) => {
+        clickStartPosition = e.clientX;
+        startPosition = e.clientX;
+        startTranslate = currentTranslate;
+        isDragging = true;
+        list.style.transition = 'none';
+    });
+    
+    list.addEventListener('mousemove', (e) => {
+        if (isDragging) {
+            const dragDistance = e.clientX - startPosition;
+            currentTranslate = startTranslate + dragDistance;
+            list.style.transform = `translateX(${currentTranslate}px)`;
+        }
+    });
+    
+    list.addEventListener('mouseup', () => {
+        if (isDragging) {
+            isDragging = false;
+            list.style.transition = 'transform 0.5s ease-in-out';
+    
+            const nearestIndex = Math.round(-currentTranslate / items[0].offsetWidth);
+            currentTranslate = -nearestIndex * items[0].offsetWidth + (list.offsetWidth / 2 - items[nearestIndex].offsetWidth / 2);
+            list.style.transform = `translateX(${currentTranslate}px)`;
+            handleItemClick(items[nearestIndex].querySelector('.item'));
+        } else {
+            const clickedIndex = Math.floor((clickStartPosition - list.getBoundingClientRect().left) / items[0].offsetWidth);
+            const centerTranslate = -clickedIndex * items[0].offsetWidth + (list.offsetWidth / 2 - items[clickedIndex].offsetWidth / 2);
+            startTranslate = centerTranslate;
+        }
+    });
+    
+    list.addEventListener('mouseleave', () => {
+        if (isDragging) {
+            isDragging = false;
+            list.style.transition = 'transform 0.5s ease-in-out';
+    
+            const nearestIndex = Math.round(-currentTranslate / items[0].offsetWidth);
+            currentTranslate = -nearestIndex * items[0].offsetWidth + (list.offsetWidth / 2 - items[nearestIndex].offsetWidth / 2);
+            list.style.transform = `translateX(${currentTranslate}px)`;
+            handleItemClick(items[nearestIndex].querySelector('.item'));
+        }
+    });
+    
+    list.addEventListener('touchstart', (e) => {
+        clickStartPosition = e.touches[0].clientX;
+        startPosition = e.touches[0].clientX;
+        startTranslate = currentTranslate;
+        isDragging = true;
+        list.style.transition = 'none';
+    });
+    
+    list.addEventListener('touchmove', (e) => {
+        if (isDragging) {
+            const dragDistance = e.touches[0].clientX - startPosition;
+            currentTranslate = startTranslate + dragDistance;
+            list.style.transform = `translateX(${currentTranslate}px)`;
+        }
+    });
+    
+    list.addEventListener('touchend', () => {
+        if (isDragging) {
+            isDragging = false;
+            list.style.transition = 'transform 0.5s ease-in-out';
+    
+            const nearestIndex = Math.round(-currentTranslate / items[0].offsetWidth);
+            currentTranslate = -nearestIndex * items[0].offsetWidth + (list.offsetWidth / 2 - items[nearestIndex].offsetWidth / 2);
+            list.style.transform = `translateX(${currentTranslate}px)`;
+            handleItemClick(items[nearestIndex].querySelector('.item'));
+        } else {
+            const clickedIndex = Math.floor((clickStartPosition - list.getBoundingClientRect().left) / items[0].offsetWidth);
+            const centerTranslate = -clickedIndex * items[0].offsetWidth + (list.offsetWidth / 2 - items[clickedIndex].offsetWidth / 2);
+            startTranslate = centerTranslate;
+        }
+    });
+    
+    list.addEventListener('touchcancel', () => {
+        if (isDragging) {
+            isDragging = false;
+            list.style.transition = 'transform 0.5s ease-in-out';
+    
+            const nearestIndex = Math.round(-currentTranslate / items[0].offsetWidth);
+            currentTranslate = -nearestIndex * items[0].offsetWidth + (list.offsetWidth / 2 - items[nearestIndex].offsetWidth / 2);
+            list.style.transform = `translateX(${currentTranslate}px)`;
+            handleItemClick(items[nearestIndex].querySelector('.item'));
+        }
+    });
+
+    items.forEach(item => {
+        const itemDiv = item.querySelector('.item');
+        itemDiv.addEventListener('click', function () {
+            handleItemClick(this);
+        });
+    });
+});
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
